8373cf6e046104605bface1c6aae766a2214ce53
Abdulwahab687 <hifigamer574@gmail.com>
2024-11-06 23:41:33 -0800

    Coding is cool.py

--- Excersize 01/Coding is cool.py
+++ Excersize 01/Coding is cool.py
@@ -1,4 +1,3 @@
-
 '''
 Exercise 1: Coding is Cool- 10 Marks
 Fill in the blanks in the Python code below to output the phrase **"Coding is Cool"** to the console using variables and string concatenation.
