2378e711c1970e81fdfa9fbf59fb682be8da19cd
Abdulwahab687 <hifigamer574@gmail.com>
2024-11-27 22:33:56 -0800

    Else in for loop.py

--- Class Practices/Else in for loop.py
+++ Class Practices/Else in for loop.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 numbers = [1, 3, 5, 7, 9]
 
 # Prompt the user to enter a number to search
@@ -11,17 +10,3 @@ for num in numbers:
         break
 else:
     print(f"{target} is not in the list.")
-=======
-numbers = [1, 3, 5, 7, 9]
-
-# Prompt the user to enter a number to search
-target = int(input("Enter a number to search for: "))
-
-# For loop with else clause
-for num in numbers:
-    if num == target:
-        print(f"{target} is found in the list!")
-        break
-else:
-    print(f"{target} is not in the list.")
->>>>>>> Stashed changes
