b7ef359a1627ad073ad6adf716230196113ed32c
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 14:20:00 -0800

    Create Strings Concatenation.py

---
+++ Z_Bonus_Excersizes/Strings Concatenation.py
@@ -0,0 +1,13 @@
+
+'''
+Write three strings in different variables and print the output as one string.
+'''
+
+# Storing three separate strings in variables
+part1 = "Hello"
+part2 = "World"
+part3 = "2024"
+
+# Combining the strings with spaces and displaying the result
+final_string = part1 + " " + part2 + " " + part3
+print(final_string)
