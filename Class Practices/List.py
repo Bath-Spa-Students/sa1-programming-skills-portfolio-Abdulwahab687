220ebf785e0be042c8dcd2b7399180d281863ddb
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 10:31:09 -0800

    Create List.py

---
+++ Class Practices/List.py
@@ -0,0 +1,25 @@
+# Homogeneous list example
+course_details = ["Sheikh Abdul Wahab", "Bathspa", "CC Year 1"]
+print(course_details)
+
+# Heterogeneous list example
+student_info = ["Wahab", 18, 4.90]
+print(student_info)
+
+# Another homogeneous list example
+locations = ["Sheikh", "UAE", "Sharjah"]
+print(locations)
+
+# Another heterogeneous list example
+teacher_info = ["Masood", 23, 3.9]
+print(teacher_info)
+
+# Using the repetition operator (*) to duplicate list items
+original_list = [1, 2, 3, 4]
+repeated_list = original_list * 3
+print(repeated_list)
+
+# Another example with repetition
+original_data = [22, 33, 44, 55]
+expanded_data = original_data * 4
+print(expanded_data)
