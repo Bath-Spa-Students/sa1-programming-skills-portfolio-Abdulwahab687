f6f908afed1dfd350da4581fbb4a2fa4612324b9
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 10:40:15 -0800

    Create Print.py

---
+++ Class Practices/Print.py
@@ -0,0 +1,8 @@
+# Assign values to the variables
+player = "Babar"
+action = "is"
+status = "batting"
+
+# Combine the variables to form a cricket-related sentence
+match_update = player + " " + action + " " + status  # This will output "Babar is batting"
+print("Current Match Update:", match_update)
