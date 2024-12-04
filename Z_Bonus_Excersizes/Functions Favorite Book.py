9fe73d6d31381a689d31bdf234d75f33326e5996
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 11:56:34 -0800

    Create Functions Favorite Book.py

---
+++ Z_Bonus_Excersizes/Functions Favorite Book.py
@@ -0,0 +1,13 @@
+
+'''
+Functions -Favorite Book:
+favorite books is Alice in Wonderland. Call the function, making sure to include a book
+title as an argument in the function call.
+'''
+
+# Defining a function to display a favorite cricketer
+def favorite_cricketer(player_name):
+    print(f"My favorite cricketer is {player_name}.")
+
+# Invoking the function with a specific cricketer's name
+favorite_cricketer("Babar Azam")
