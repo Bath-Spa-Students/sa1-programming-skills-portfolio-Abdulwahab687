cf8b1e69523c3e427ab97ba0871e0f04f7274a44
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 10:53:20 -0800

    Create Turtle Graphics.py

---
+++ Class Practices/Turtle Graphics.py
@@ -0,0 +1,11 @@
+import turtle
+
+# Draw a red circle
+turtle.pensize(4)  # Set pen size
+turtle.pencolor('Blue')  # Set pen color
+turtle.circle(120)  # Draw a circle with a radius of 120
+
+# Draw a yellow circle
+turtle.pensize(4)  # Set pen size
+turtle.pencolor('Black')  # Change pen color to yellow
+turtle.circle(160)  # Draw a circle with a radius of 90
