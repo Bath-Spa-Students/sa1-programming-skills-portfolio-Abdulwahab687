8130df055480cf90fe994d0abe9d6eb34e79ddd1
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 10:30:50 -0800

    Create Elif.py

---
+++ Class Practices/Elif.py
@@ -0,0 +1,10 @@
+# Comparing two numbers using if-elif-else
+num1 = 400
+num2 = 400
+
+if num2 > num1:
+    print("num2 is larger than num1")
+elif num1 == num2:
+    print("num1 and num2 are equal")
+else:
+    print("num1 is larger than num2")
