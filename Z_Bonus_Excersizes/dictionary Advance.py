d3c05dd845d70c2ff61dc85ce37b1cde47d0e6e2
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 11:35:26 -0800

    Create dictionary Advance.py

---
+++ Z_Bonus_Excersizes/dictionary Advance.py
@@ -0,0 +1,13 @@
+# Creating a dictionary to store personal details
+user_details = {
+    "first_name": "Nasir",
+    "last_name": "Ali",
+    "age": 42,
+    "city": "Dubai"
+}
+
+# Outputting each piece of information from the dictionary
+print("First Name:", user_details["first_name"])
+print("Last Name:", user_details["last_name"])
+print("Age:", user_details["age"])
+print("City:", user_details["city"])
