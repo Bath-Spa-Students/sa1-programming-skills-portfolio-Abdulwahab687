7fa5c5a90d523936346874cd4063ff448ca95d35
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 14:10:07 -0800

    Create List Names.py

---
+++ Z_Bonus_Excersizes/List Names.py
@@ -0,0 +1,14 @@
+
+'''
+Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.
+'''
+
+# Defining a list to store friends' names
+friends = ["Abdul Wahab", "Rayan", "Bisma", "Soha"]
+
+# Displaying each friend's name by accessing the list elements individually
+print(friends[0])  # Name at index 0
+print(friends[1])  # Name at index 1
+print(friends[2])  # Name at index 2
+print(friends[3])  # Name at index 3
