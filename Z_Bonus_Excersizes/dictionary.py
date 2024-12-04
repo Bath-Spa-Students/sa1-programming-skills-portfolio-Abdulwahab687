002ae11b05ad57abc41797f1e74ac54fa8c203bf
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 11:24:06 -0800

    Create dictionary.py

---
+++ Z_Bonus_Excersizes/dictionary.py
@@ -0,0 +1,13 @@
+# Defining a dictionary to hold details about an individual
+individual_info = {
+    "first_name": "Sheikh",
+    "last_name": "Abdul Wahab Nasir Ali",
+    "age": 18,
+    "city": "Sharjah"
+}
+
+# Displaying the stored information
+print("First Name:", individual_info["first_name"])
+print("Last Name:", individual_info["last_name"])
+print("Age:", individual_info["age"])
+print("City:", individual_info["city"])
