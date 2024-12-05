a2e418edccdeed68d9ce4be4f153848e5b9a4772
Abdulwahab687 <hifigamer574@gmail.com>
2024-12-04 23:36:37 -0800

    Update Variables.py

---
+++ Z_Bonus_Excersizes/Variables.py
@@ -0,0 +1,13 @@
+
+'''
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.
+'''
+
+# Initial message assignment
+greeting = "Welcome to the world of Python programming!"
+print(greeting)
+
+# Updating the message with a new value
+new_message = "Dive in and enjoy learning, building, and exploring with Python!"
+print(new_message)
